893abd9c9a5015eccd371fd268402e7b2376c8e5
Th1nhNg0 <ngophuthinh2001@gmail.com>
2022-10-06 22:08:58 +0700

    update

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -28,8 +28,8 @@ function Nav() {
   return (
     <div className="relative z-20 p-10 w-96">
       <Link href="/">
-        <a className="text-4xl font-bold uppercase text-theme-paragraphs">
-          Embery
+        <a className="text-4xl font-bold uppercase theme-font text-theme-paragraphs">
+          Luigi&apos;s italian restaurant
         </a>
       </Link>
       <ul className="mt-5 space-y-5 font-semibold text-theme-paragraphs">
