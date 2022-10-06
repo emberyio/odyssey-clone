9a4129182ed10d75ec9a621f658b314c29ba4557
Th1nhNg0 <ngophuthinh2001@gmail.com>
2022-10-06 23:43:10 +0700

    done

--- pages/blog.tsx
+++ pages/blog.tsx
@@ -4,7 +4,7 @@ export default function Blog() {
   return (
     <div className="min-h-full p-5 lg:p-10 bg-theme-dark">
       <div className="flex justify-between ">
-        <h1 className=" heading text-theme-accent text-[10vw] pr-5 leading-[98%]">
+        <h1 className="pr-5 text-3xl lg:text-6xl menu-font text-theme-accent">
           BLOG AND EVENTS
         </h1>
       </div>
@@ -12,7 +12,7 @@ export default function Blog() {
       <div className="my-10 space-y-5">
         {Array.from({ length: 10 }).map((_, i) => (
           <div
-            className="flex pt-5 pb-10 border-b-[1px] border-gray-500/50"
+            className="flex flex-col lg:flex-row pt-5 pb-10 border-b-[1px] border-gray-500/50"
             key={i}
           >
             <div className="flex-1">
@@ -22,7 +22,7 @@ export default function Blog() {
                 className="object-cover w-full h-full rounded-xl aspect-video"
               />
             </div>
-            <div className="flex-1 pl-5">
+            <div className="flex-1 pt-5 lg:pl-5 lg:pt-0">
               <div className="text-xs text-theme-accent">MAY 23, 2022</div>
               <div className="mb-5 text-3xl font-semibold text-theme-paragraphs">
                 Can We Stop Treating Restaurants As Status Symbols?
