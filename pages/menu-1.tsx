7ecc83bd3bb0faa5750e4b08a25064a5f68c7bb5
Th1nhNg0 <ngophuthinh2001@gmail.com>
2022-10-14 20:12:17 +0700

    update

--- pages/menu-1.tsx
+++ pages/menu-1.tsx
@@ -1,336 +1,19 @@
+import { GetStaticProps } from "next";
 import React from "react";
+import { parse } from "csv-parse";
+import fs from "fs";
 
-const items = [
-  {
-    name: "Bruschetta al Pomodoro",
-    description:
-      "tomato, fresh basil, olive oil & red onion on toasted pane di casa bread",
-    price: 16,
-  },
-  {
-    name: "Insalata di Finocchio e Arancio",
-    description:
-      "Fennel, orange, rocket, olive oil, balsamic vinegar & cracked pepper",
-    price: 18,
-  },
-  {
-    name: "Fish & Chips",
-    description: "Beer battered flathead tails with steak chips and salad",
-    price: 20,
-  },
-  {
-    name: "Beef Burger",
-    description:
-      "House-made wagyu beef pattie, lettuce, American cheese, maple bacon, egg & relish, served w/ steak chips",
-    price: 22,
-  },
-  {
-    name: "Schnitzel Burger",
-    description:
-      "Crumbed chicken breast, cheese, lettuce, tomato & chipolte mayo,served w/ steak chips",
-    price: 22,
-  },
-  {
-    name: "Risotto ai Funghi",
-    description: "Spinach, mushrooms, garlic, parmesan & herbs",
-    price: 25,
-  },
-  {
-    name: "Gnocchi al Pesto",
-    description:
-      "Pan-tossed gnocchi in a fresh basil pesto sauce w/ cherry tomatoes & grana padano",
-    price: 25,
-  },
-  {
-    name: "Salt & Pep er Calamari",
-    description:
-      "Served on a continental salad w/ olive oil & balsamic vinegar",
-    price: 25,
-  },
-  {
-    name: "Spaghetti alle Vongole",
-    description:
-      "Clams pan-tossed in a garlic & white wine sauce w/ olive oil & parsley",
-    price: 26,
-  },
-];
+interface MenuItem {
+  name: string;
+  description: string;
+  price: number;
+}
+interface Menu {
+  title: string;
+  items: MenuItem[];
+}
 
-const items2 = [
-  {
-    name: "Caprese con Bufala",
-    description: "sliced tomatoes w/ buffalo mozzarella, olive oil & balsamic",
-    price: 26,
-  },
-  {
-    name: "Tre Sapori",
-    description: "an assortment ofItalian meatballs, arancini & bruschetta",
-    price: 30,
-  },
-  {
-    name: "Antipasto della Casa",
-    description:
-      "Italian meats, marinated vegetables, cheese, olives & focaccia bread",
-    price: 32,
-  },
-  {
-    name: "Misto di Pesce",
-    description:
-      "shallow fried calamari, garlic prawns & sauteed mussels served w/toasted bread",
-    price: 34,
-  },
-];
-
-const items3 = [
-  {
-    name: "Bruschetta al Pomodoro",
-    description: "tomato, basil, red onion & olive oil served on toasted bread",
-    price: 20,
-  },
-  {
-    name: "Polpette di Carne",
-    description:
-      "Italian meatballs in a thick Napoli sauce served w/toasted bread",
-    price: 21,
-  },
-  {
-    name: "Arancini di Riso",
-    description:
-      "rice balls filled w/ cheese & Bolognese sauce, crumbed & fried",
-    price: 21,
-  },
-  {
-    name: "Cozze al Pomodoro",
-    description:
-      "mussels served in a tomato, white wine, chilli & garlic sauce",
-    price: 25,
-  },
-  {
-    name: "Calamari Fritti",
-    description:
-      "shallow fried calamari served w/ a fennel, orange & rocket salad w/ vinaigrette",
-    price: 25,
-  },
-  {
-    name: "Calamari alla Griglia",
-    description:
-      "grilled calamari on a continental salad w/ olive oil & balsamic vinegar",
-    price: 27,
-  },
-  {
-    name: "Gamberi all''Aglio",
-    description:
-      "grilled prawns w/ garlic, chilli & white wine served w/ pane di casa bread",
-    price: 28,
-  },
-  {
-    name: "Insalata di Vitello",
-    description:
-      "tender eye fillet, grilled & served on a gourmet salad w/ honey soy dressing",
-    price: 30,
-  },
-];
-const items4 = [
-  {
-    name: "Melenzana Parmigiana",
-    description:
-      "grilled eggplant, cheese & Napoli sauce served w/ potatoes & veggies",
-    price: 28,
-  },
-  {
-    name: "Pollo alla Parmigiana",
-    description:
-      "crumbed chicken breast w/ cheese & Napoli sauce, potatoes & veggies",
-    price: 30,
-  },
-  {
-    name: "Pollo Saltimbocca",
-    description:
-      "chicken, sage, prosciutto, white wine, lemon & caper sauce, potatoes & veggies",
-    price: 34,
-  },
-  {
-    name: "Tagliata di Manzo",
-    description:
-      "grilled eye fillet of beef w/ red wine jus, served w/ potatoes & veggies",
-    price: 45,
-  },
-];
-const items5 = [
-  {
-    name: "Patatine",
-    description:
-      "thick cut potato chips, served w/ a choice of sauce - relish, aioli,tomato, chipolte or mayo",
-    price: 10,
-  },
-  {
-    name: "Patate Arrostite",
-    description: "Italian roasted potatoes w/ sea salt & rosemary",
-    price: 12,
-  },
-  {
-    name: "Verdure di Stagione",
-    description: "seasonal steamed veggies",
-    price: 15,
-  },
-  {
-    name: "Insalata Mista",
-    description: "seasonal steamed veggies",
-    price: 16,
-  },
-  {
-    name: "Insalata di Rucola",
-    description: "seasonal steamed veggies",
-    price: 17,
-  },
-];
-const items6 = [
-  {
-    name: "Linguine allo Scoglio",
-    description: "pancetta, egg, parmesan, pecorino cheese & a touch of cream",
-    price: 32,
-  },
-  {
-    name: "Spaghetti alla Carbonara",
-    description: "pancetta, egg, parmesan, pecorino cheese & a touch of cream",
-    price: 28,
-  },
-  {
-    name: "Gnocchi al Pomodoro",
-    description: "potato gnocchi in a rich Napoli sauce w/fresh basil",
-    price: 29,
-  },
-  {
-    name: "Spaghetti con Polpette",
-    description: "spaghetti w/traditional Italian meatballs in a Napoli sauce",
-    price: 29,
-  },
-  {
-    name: "Ravioli",
-    description:
-      "(Pumpkin & Ricotta or Duck) in a burnt butter & sage sauce w/ red wine jus & parmesan",
-    price: 30,
-  },
-  {
-    name: "Rigatoni all 'Amatriciana",
-    description:
-      "pancetta, garlic, cherry tomatoes & basil in a mild chilli Napoli sauce",
-    price: 29,
-  },
-
-  {
-    name: "Penne all 'Arrabbiata",
-    description:
-      "cherry tomatoes, garlic, hot chilli, olives, parsley & a touch of Napoli",
-    price: 28,
-  },
-  {
-    name: "Lasagna al Forno",
-    description: "layers oflasagna sheets filled w/Bolognese & bechamel sauce",
-    price: 32,
-  },
-  {
-    name: "Risotto con Pollo e Funghi",
-    description: "rise, chicken, spinach, mushrooms & parmesan",
-    price: 29,
-  },
-  {
-    name: "Risotto con Gamberi",
-    description:
-      "rice, prawns, cherry tomatoes, garlic, oilve oil, white wine, spinach & lemon",
-    price: 32,
-  },
-];
-const items7 = [
-  {
-    name: "Focaccia Aglio e Olio",
-    description: "garlic, mozzarella, olive oil, sea salt & rosemary",
-    price: 13,
-  },
-  {
-    name: "Margherita",
-    description: "tomato sauce, mozzarella,fresh basil & olive oil",
-    price: 21,
-  },
-  {
-    name: "Diavola",
-    description: "tomato sauce, mozzarella, hot salami & fresh basil",
-    price: 23,
-  },
-  {
-    name: "Napoletana",
-    description:
-      "tomato sauce, mozzarella, olives, cherry tomatoes, capers, anchovies & oregano",
-    price: 25,
-  },
-  {
-    name: "Giardiniera",
-    description:
-      "tomato sauce, mozzarella, zucchini, eggplant, cherry tom, red onion, basil & Parmesan",
-    price: 26,
-  },
-  {
-    name: "Salsiccia",
-    description:
-      "tomato sauce, mozzarella, Italian pork & fennel sausage and chilli",
-    price: 26,
-  },
-  {
-    name: "Pollo e Zucca",
-    description:
-      "tomato sauce, mozzarella, roasted chicken, pumpkin, spinach, pine nuts & olive oil",
-    price: 27,
-  },
-  {
-    name: "Capricciosa",
-    description:
-      "tomato sauce, mozzarella, ham, mushrooms, hot salami, olives & oregano",
-    price: 28,
-  },
-  {
-    name: "Prosciutto e Rucola",
-    description:
-      "tomato sauce, mozzarella, prosciutto, rocket, Parmesan cheese & olive oil",
-    price: 29,
-  },
-  {
-    name: "Amalfi",
-    description: "tomato sauce, mozzarella, prawns, garlic & cherry tomatoes",
-    price: 30,
-  },
-];
-const items8 = [
-  {
-    name: "Kids Margherita Pizza",
-    description: "",
-    price: 14,
-  },
-  {
-    name: "Fish & Chips",
-    description: "",
-    price: 14,
-  },
-  {
-    name: "Chicken Schnitzel & Chips",
-    description: "",
-    price: 14,
-  },
-  {
-    name: "Pasta Bolognese",
-    description: "",
-    price: 14,
-  },
-  {
-    name: "Spaghetti & Meatballs",
-    description: "",
-    price: 14,
-  },
-  {
-    name: "Mac & Cheese",
-    description: "",
-    price: 14,
-  },
-];
-export default function menu() {
+export default function MenuPage({ menus }: { menus: Menu[] }) {
   return (
     <div className="p-5 lg:p-10 bg-theme-dark">
       <div className="flex items-center justify-between ">
@@ -345,18 +28,9 @@ export default function menu() {
       </div>
       <div className="w-full h-1 border-t-[1px] border-b-[1px] border-gray-500/50 mt-8"></div>
       <div className="mt-10 space-y-10">
-        <Menu title="Lunch menu" items={items} />
-        <Menu title="Antipasti" items={items2} />
-        <Menu title="Primi Piatti" items={items3} />
-        <Menu title="Secondi Piatti" items={items4} />
-        <Menu title="Contorni" items={items5} />
-        <Menu title="Paste e Risotti" items={items6} />
-        <Menu title="Pizze" items={items7} />
-        <Menu
-          title="Bambini"
-          desc="KIDS UNDER 12 - ALL MEALS $14"
-          items={items8}
-        />
+        {menus.map((menu) => (
+          <Menu key={menu.title} {...menu} />
+        ))}
       </div>
     </div>
   );
@@ -397,7 +71,7 @@ function Menu({
               <td className="pt-5 pb-5 pr-10 text-gray-500 border-b-[1px] border-gray-500/50  border-dotted">
                 {item.description.toLowerCase()}
               </td>
-              <td className="pt-5 pb-5 border-b-[1px] border-dotted border-gray-500/50  text-theme-paragraphs">
+              <td className="pt-5 text-right pb-5 border-b-[1px] border-dotted border-gray-500/50  text-theme-paragraphs">
                 ${item.price}
               </td>
             </tr>
@@ -407,3 +81,31 @@ function Menu({
     </div>
   );
 }
+
+export const getStaticProps: GetStaticProps = async (context) => {
+  const files = fs.readdirSync("./data/dinein");
+  const menus: Menu[] = await Promise.all(
+    files.map(async (file) => {
+      const data = fs.readFileSync(`./data/dinein/${file}`, "utf8");
+      const records = await new Promise((resolve, reject) => {
+        parse(data, { columns: true }, (err, records) => {
+          if (err) reject(err);
+          resolve(records);
+        });
+      });
+      const titleRaw = file.replace(".csv", "").split("_");
+      titleRaw.shift();
+      const title = titleRaw.join(" ");
+
+      return {
+        title,
+        items: records as MenuItem[],
+      };
+    })
+  );
+  return {
+    props: {
+      menus,
+    },
+  };
+};
--- pages/menu-2.tsx
+++ pages/menu-2.tsx
@@ -1,250 +1,19 @@
+import { parse } from "csv-parse";
+import { GetStaticProps } from "next";
 import React from "react";
+import fs from "fs";
 
-const items1 = [
-  {
-    name: "TIRAMISU'",
-    description:
-      "a Tuscan style trifle w/ mascarpone,\nespresso coffee, strega & marsala liquored sponge",
-    price: 15.0,
-  },
-  {
-    name: "NUTELLA PIZZA",
-    description:
-      "wood-fired pizza base smothered\nw/ Nutella and fresh strawberries (add ice-cream $2)",
-    price: 15.0,
-  },
-  {
-    name: "SICILIAN CANNOLI",
-    description:
-      "a cannoli shell filled w/\nchocolate-ricotta & pistacchio-ricotta",
-    price: 6.0,
-  },
-];
-const items2 = [
-  {
-    name: "GAMBERI ALL'AGLIO",
-    description:
-      "grilled prawns w/ garlic, chilli & white wine served w/ pane di casa bread\u00a0",
-    price: 28.0,
-  },
-  {
-    name: "POLPETTE DI CARNE ",
-    description:
-      "Italian meatballs in a thick Napoli\nsauce served w/ pane di casa bread",
-    price: 21.0,
-  },
-  {
-    name: "ARANCINI DI RISO",
-    description:
-      "rice balls, filled w/ cheese &\nBolognese sauce, crumbed & fried",
-    price: 21.0,
-  },
-  {
-    name: "CALAMARI ALLA GRIGLIA ",
-    description:
-      "shallow fried calamari served w/ a\nfennel, orange & rocket salad w/ vinaigrette",
-    price: 25.0,
-  },
-  {
-    name: "INSALATA DI VITELLO",
-    description:
-      "tender eye fillet, grilled &\nserved on a gourmet salad w/ honey soy dressing",
-    price: 30.0,
-  },
-  {
-    name: "CALAMARI ALLA GRIGLIA",
-    description:
-      "seasoned tender grilled\ncalamari served on a continental salad w/ olive oil &\nbalsamic vinegar",
-    price: 27.0,
-  },
-];
+interface MenuItem {
+  name: string;
+  description: string;
+  price: number;
+}
+interface Menu {
+  title: string;
+  items: MenuItem[];
+}
 
-const items3 = [
-  {
-    name: "PATATINE",
-    description:
-      "Thick cut potato chips served w/\nrelish, aioli, tomato, chipolte or mayo",
-    price: 10.0,
-  },
-  {
-    name: "PATATE ARROSTITE",
-    description: "Italian roasted potatoes w/\nsea salt and rosemary",
-    price: 12.0,
-  },
-  {
-    name: "INSALATA MISTA",
-    description:
-      "mixed lettuce, cherry tomatoes,\ncucumber, red onion & vinaigrette dressing",
-    price: 16.0,
-  },
-  {
-    name: "INSALATA DI RUCOLA",
-    description:
-      "rocket, walnuts, shaved\nparmesan, balsamic vinegar & olive oil",
-    price: 17.0,
-  },
-  {
-    name: "VERDURE DI STAGIONE",
-    description: "seasonal steamed veggies",
-    price: 15.0,
-  },
-];
-
-const items4 = [
-  {
-    name: "FOCACCIA AGLIO E OLIO",
-    description: "garlic, mozzrella cheese,\nolive oil, sea salt & rosemary",
-    price: 13.0,
-  },
-  {
-    name: "MARGHERITA",
-    description: "tomato sauce, mozzarella cheese, fresh\nbasil & olive oil",
-    price: 21.0,
-  },
-  {
-    name: "PROSCIUTTO E RUCOLA",
-    description:
-      "tomato sauce, mozzarella,\nprosciutto, rocket, parmesan cheese & olive oil",
-    price: 29.0,
-  },
-  {
-    name: "POLLO E ZUCCA",
-    description:
-      "tomato, sauce, mozzarella, roasted\nchicken, pumpkin, spinach, pine nuts & olive oil",
-    price: 27.0,
-  },
-  {
-    name: "DIAVOLA",
-    description: "tomato, mozzarella, hot salami & fresh basil",
-    price: 23.0,
-  },
-  {
-    name: "CAPRICCIOSA",
-    description:
-      "tomato sauce, mozzarella, ham,\nmushrooms, hot salami, olives & fresh oregano",
-    price: 28.0,
-  },
-  {
-    name: "GIARDINIERA",
-    description:
-      "tomato sauce, mozzarella, zucchini,\neggplant, cherry tomatoes, red onion, basil & parmesan",
-    price: 26.0,
-  },
-  {
-    name: "SALSICCIA",
-    description:
-      "tomato sauce, mozzarella, Italian pork &\nfennel sausage & chilli",
-    price: 26.0,
-  },
-  {
-    name: "NAPOLETANA",
-    description:
-      "tomato sauce, mozzarella, olives,\ncherry tomatoes, capers, anchovies & oregano",
-    price: 25.0,
-  },
-  {
-    name: "AMALFI",
-    description: "tomato sauce, mozzarella, prawns, garlic &\ncherry tomatoes",
-    price: 30.0,
-  },
-];
-
-const items5 = [
-  {
-    name: "LINGUINE ALLO SCOGLIO",
-    description:
-      "prawns, calamari, scallops,\nmussels, cherry tomatoes, garlic, olive oil & Napoli sauce",
-    price: 32.0,
-  },
-  {
-    name: "SPAGHETTI ALLA CARBONARA",
-    description: "pancetta, egg,\nparmesan, pecorino cheese & a touch of cream",
-    price: 29.0,
-  },
-  {
-    name: "GNOCCHI AL POMODORO",
-    description: "potato gnocchi in a rich\nNapoli sauce w/ fresh basil",
-    price: 29.0,
-  },
-  {
-    name: "SPAGHETTI CON POLPETTE",
-    description:
-      "spaghetti w/ traditional\nItalian meatballs in a Napoli sauce",
-    price: 29.0,
-  },
-  {
-    name: "RIGATONI ALLA BOLOGNESE",
-    description: "pasta in a traditional\nItalian Bolognese sauce",
-    price: 28.0,
-  },
-  {
-    name: "PENNE ALL' ARRABIATA",
-    description:
-      "cherry tomatoes, garlic, hot\nchilli, olives, parsley & a touch of Napoli",
-    price: 28.0,
-  },
-  {
-    name: "RIGATONI ALL'AMATRICIANA",
-    description:
-      "pancetta, garlic cherry\ntomatoes & basil in a mild chilli Napoli sauce",
-    price: 29.0,
-  },
-  {
-    name: "LASAGNA AL FORNO",
-    description:
-      "layers of lasagna sheets filled w/\nBolognese & bechamel sauce",
-    price: 27.0,
-  },
-  {
-    name: "RISOTTO CON POLLO E FUNGHI",
-    description: "rice, chicken, spinacH,\nmushrooms & parmesan",
-    price: 29.0,
-  },
-  {
-    name: "RISOTTO GAMBERI",
-    description:
-      "rice, prawns, cherry tomatoes,\ngarlic, olive oil, white wine, spinach & lemon",
-    price: 32.0,
-  },
-];
-const items6 = [
-  {
-    name: "POLLO ALLA PARMIGIANA",
-    description:
-      "crumbed chicken breast w/\ncheese & Napoli sauce, potatoes & veggies",
-    price: 29.0,
-  },
-  {
-    name: "MELENZANA PARMIGIANA",
-    description:
-      "grilled eggplant, cheese &\nNapoli sauce served w/ potatoes & veggies",
-    price: 32.0,
-  },
-];
-const items7 = [
-  {
-    name: "375ml Soft Drink (Coke, No Sugar, Sprite, Lift, Fanta)",
-    description: "",
-    price: 3.5,
-  },
-  {
-    name: "1.25lt Soft Drink $5\n(Coke, No Sugar, Sprite,)",
-    description: "",
-    price: 5.0,
-  },
-  {
-    name: "600ml Mount Franklin Still Water",
-    description: "",
-    price: 3.5,
-  },
-  {
-    name: "450ml Mount Franklin Lightly Sparkling Water",
-    description: "",
-    price: 3.5,
-  },
-];
-
-export default function menu() {
+export default function MenuPage({ menus }: { menus: Menu[] }) {
   return (
     <div className="p-5 lg:p-10 bg-theme-dark">
       <div className="flex items-center justify-between ">
@@ -274,13 +43,9 @@ export default function menu() {
       </div>
       <div className="w-full h-1 border-t-[1px] border-b-[1px] border-gray-500/50 mt-8"></div>
       <div className="mt-10 space-y-10">
-        <Menu title="STARTERS" items={items2} />
-        <Menu title="PASTA & RISOTTO" items={items5} />
-        <Menu title="MAINS" items={items6} />
-        <Menu title="PIZZAS" items={items4} />
-        <Menu title="SIDES" items={items3} />
-        <Menu title="SWEETS" items={items1} />
-        <Menu title="DRINKS" items={items7} />
+        {menus.map((menu) => (
+          <Menu key={menu.title} {...menu} />
+        ))}
       </div>
     </div>
   );
@@ -317,7 +82,7 @@ function Menu({
                 </div>
               </td>
 
-              <td className="pt-5 pb-5 border-b-[1px] border-dotted border-gray-500/50  text-theme-paragraphs">
+              <td className="pt-5 pb-5 text-right border-b-[1px] border-dotted border-gray-500/50  text-theme-paragraphs">
                 ${item.price}
               </td>
             </tr>
@@ -327,3 +92,31 @@ function Menu({
     </div>
   );
 }
+
+export const getStaticProps: GetStaticProps = async (context) => {
+  const files = fs.readdirSync("./data/dinein");
+  const menus: Menu[] = await Promise.all(
+    files.map(async (file) => {
+      const data = fs.readFileSync(`./data/dinein/${file}`, "utf8");
+      const records = await new Promise((resolve, reject) => {
+        parse(data, { columns: true }, (err, records) => {
+          if (err) reject(err);
+          resolve(records);
+        });
+      });
+      const titleRaw = file.replace(".csv", "").split("_");
+      titleRaw.shift();
+      const title = titleRaw.join(" ");
+
+      return {
+        title,
+        items: records as MenuItem[],
+      };
+    })
+  );
+  return {
+    props: {
+      menus,
+    },
+  };
+};
