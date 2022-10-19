1091617f9ca0c428893f37f2e7da585652c11788
minh-git <minh.nguyenvonhat@gmail.com>
2022-10-19 07:59:23 +0700

    changes

--- pages/photo.tsx
+++ pages/photo.tsx
@@ -1,7 +1,17 @@
 import Image from "next/image";
 import React from "react";
 
+
+
 export default function Blog() {
+  const PhotoItems = [{
+    "title": "GNOCCHI CON SALSICCIA E FUNGHI PORCINI",
+    "desc": "Potato gnocchi with Italian pork & fennel sausage, porcini mushrooms, white wine, rocket and shave Parmesan"
+  },
+  { "title": "ANTIPASTO DELLA CASA", 
+  "desc": "A selection of cold Italian cured meats, marinated vegetables, a selection of cheese, olives & buffalo mozzarella." }]
+
+
   return (
     <div className="min-h-full p-5 lg:p-10 bg-theme-dark">
       <div className="flex justify-between ">
@@ -11,7 +21,7 @@ export default function Blog() {
       </div>
       <div className="w-full h-1 border-t-[1px] border-b-[1px] border-gray-500/50 mt-8"></div>
       <div className="my-10 space-y-5">
-        {Array.from({ length: 2 }).map((_, i) => (
+        {PhotoItems.map((PhotoItem, i) => (
           <div
             className="flex flex-col lg:flex-row pt-5 pb-10 border-b-[1px] border-gray-500/50"
             key={i}
@@ -26,9 +36,9 @@ export default function Blog() {
             </div>
             <div className="flex-1 pt-5 lg:pl-5 lg:pt-0">
               <div className="mb-5 text-3xl font-semibold text-theme-paragraphs">
-                Our food
+                {PhotoItem.title}
               </div>
-              <div className="text-gray-500">The best in town</div>
+              <div className="text-gray-500">{PhotoItem.desc}</div>
             </div>
           </div>
         ))}
