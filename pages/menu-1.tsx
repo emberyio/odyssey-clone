7db17506a50c5269f6315e45444d832e54a236c1
Minh Nguyen <minh.nguyenvonhat@gmail.com>
2026-05-09 12:33:15 +1000

    update

--- pages/menu-1.tsx
+++ pages/menu-1.tsx
@@ -78,16 +78,13 @@ export default function MenuPage() {
             <br />
             Please notify your waitstaff of any food intolerances or allergies.
           </p>
-          <Link href={`https://dashboard.embery.com.au/assets/${MenuFiles[0]?.file}`}>
-            <a className="flex justify-center px-8 py-4 font-semibold duration-300 rounded-full bg-theme-accent hover:bg-white">
-              {MenuFiles[0]?.name}
-            </a>
-          </Link>
-          <Link href={`https://dashboard.embery.com.au/assets/${MenuFiles[1]?.file}`}>
-            <a className="flex justify-center px-8 py-4 font-semibold duration-300 rounded-full border-theme-accent border text-theme-accent hover:bg-white">
-            {MenuFiles[1]?.name}
-            </a>
-          </Link>
+          {MenuFiles.map((menuFile, index) => (
+            <Link key={menuFile.id} href={`https://dashboard.embery.com.au/assets/${menuFile.file}`}>
+              <a className={`flex justify-center px-8 py-4 font-semibold duration-300 rounded-full ${index === 0 ? "bg-theme-accent hover:bg-white" : "border-theme-accent border text-theme-accent hover:bg-white"}`}>
+                {menuFile.name}
+              </a>
+            </Link>
+          ))}
         </div>
       </div>
       {/* <div className="w-full h-1 border-t-[1px] border-b-[1px] border-gray-500/50 mt-8"></div> */}
